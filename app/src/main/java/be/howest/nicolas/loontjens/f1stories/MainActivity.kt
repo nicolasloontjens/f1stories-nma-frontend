d5b9d51d0ece1dfa8734e695753adbbe3d982748
NicolasLoontjens <nicolas.loontjens@student.howest.be>
2022-05-24 15:13:05 +0200

    added bottom navbar

--- app/src/main/java/be/howest/nicolas/loontjens/f1stories/MainActivity.kt
+++ app/src/main/java/be/howest/nicolas/loontjens/f1stories/MainActivity.kt
@@ -1,14 +1,23 @@
 package be.howest.nicolas.loontjens.f1stories
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import be.howest.nicolas.loontjens.f1stories.addrace.ScanCodeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var bottomNavigationView : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +25,35 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment))
+        bottomNavigationView = findViewById(R.id.bottomnavview)
+        bottomNavigationView.background = null
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.homebottomnav -> {
+                    navController.navigate(R.id.homeFragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.addracebottomnav -> {
+                    val intent = Intent (this, ScanCodeActivity::class.java)
+                    startActivity(intent)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.createstorybottomnav -> {
+                    navController.navigate(R.id.addPostFragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+        }
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
 }
