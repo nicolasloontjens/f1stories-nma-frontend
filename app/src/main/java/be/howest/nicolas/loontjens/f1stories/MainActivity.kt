ba5f31da6871fa801afa0516b4b7ddccf83deaba
NicolasLoontjens <nicolas.loontjens@student.howest.be>
2022-05-27 17:04:25 +0200

    fixed bottom navigation + added profile option

--- app/src/main/java/be/howest/nicolas/loontjens/f1stories/MainActivity.kt
+++ app/src/main/java/be/howest/nicolas/loontjens/f1stories/MainActivity.kt
@@ -7,17 +7,25 @@ import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.navigation.NavController
+import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import be.howest.nicolas.loontjens.f1stories.addrace.ScanCodeActivity
+import be.howest.nicolas.loontjens.f1stories.database.UserRoomDatabase
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var bottomNavigationView : BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,11 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment))
         bottomNavigationView = findViewById(R.id.bottomnavview)
+        findViewById<ActionMenuItemView?>(R.id.homebottomnav).visibility = View.INVISIBLE
+        findViewById<ActionMenuItemView?>(R.id.createstorybottomnav).visibility = View.INVISIBLE
+        findViewById<ActionMenuItemView?>(R.id.addracebottomnav).visibility = View.INVISIBLE
+        findViewById<ActionMenuItemView?>(R.id.profilebottomnav).visibility = View.INVISIBLE
+
         bottomNavigationView.background = null
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
@@ -43,11 +56,28 @@ class MainActivity : AppCompatActivity() {
                     navController.navigate(R.id.addPostFragment)
                     return@setOnNavigationItemSelectedListener true
                 }
-
+                R.id.profilebottomnav -> {
+                    val uid = getUid()
+                    val bundle = Bundle()
+                    if (uid != null) {
+                        bundle.putInt("uid", uid)
+                    }
+                    navController.navigate(R.id.profileFragment, bundle)
+                    return@setOnNavigationItemSelectedListener true
+                }
             }
             false
         }
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    fun getUid(): Int?{
+        var uid: Int? = null
+        CoroutineScope(Dispatchers.IO).launch {
+            uid = UserRoomDatabase.INSTANCE?.UserDao()?.getUid()
+        }
+        Thread.sleep(700)
+        return uid
     }
 
 
